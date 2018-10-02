d095a831300f5cfb699658b04b49a59ba8202db3
Matt Belisle <matthew.belisle@workiva.com>
2018-10-02 11:29:50 -0500

    WIP

--- 2018-10-02-rename/rename.py
+++ 2018-10-02-rename/rename.py
@@ -6,12 +6,12 @@ import os
 mv_ops = [] # [(src1, dest1), (src2, dest2), ...]
 
 for fname in os.listdir():
-    if os.isdir(fname):
+    if os.path.isdir(fname):
         for fname2 in os.listdir(fname):
+            fname2 = os.path.join(fname, fname2)
             suffix = ' SHP'
-            if os.isdir(fname2) and fname2.endswith(suffix):
-                mv_ops.append((fname2, fname[:len(suffix)]))
-                rm_ops.append(fname2[:len(suffix)])
+            if os.path.isdir(fname2) and fname2.endswith(suffix):
+                mv_ops.append((fname2, fname2[:len(suffix)]))
 
 for src, dest in mv_ops:
     os.renames(src, dest)
